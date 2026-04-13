69f585a64f1413e22f7577c42a372aad51acde10
Sharmila <sharmila@Sharmilas-MacBook-Pro.local>
2026-04-13 13:47:33 -0400

    sc

--- src/main/java/com/opencart/qa/utils/AppConstants.java
+++ src/main/java/com/opencart/qa/utils/AppConstants.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class AppConstants {
 
-	public static final String LOGIN_PAGE_TITLE= "Account Login11";
+	public static final String LOGIN_PAGE_TITLE= "Account Login";
 	public static final String LOGIN_PAGE_URLVALUE= "route=account/login";
 	public static final String LOGIN_PAGE_URL = "https://naveenautomationlabs.com/opencart/index.php?route=account/login";
 	public static final String HOME_PAGE_TITLE= "My Account";
